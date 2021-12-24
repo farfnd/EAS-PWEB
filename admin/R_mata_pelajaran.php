219bc45d7445ea8dff8d5feec9a454bedd25eaf1
Farhan <70105993+farfnd@users.noreply.github.com>
2021-12-24 23:38:55 +0800

    jadwal pelajaran

--- admin/R_mata_pelajaran.php
+++ admin/R_mata_pelajaran.php
@@ -8,36 +8,20 @@ header("Access-Control-Allow-Methods: *");
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     if($_GET['type'] == "all") {
-        $query = mysqli_query($db, "SELECT * FROM mata_pelajaran");
+        $query = mysqli_query($db, "SELECT * FROM jadwal_pelajaran");
         $data = array();
         while ($mapel = mysqli_fetch_array($query)) {
             array_push($data, [
                 "id" => $mapel["id"],
-                "nama_mapel" => $mapel["nama_mapel"],
-                "kode_mapel" => $mapel["kode_mapel"],
+                "mata_pelajaran" => $mapel["mata_pelajaran"],
+                "tanggal" => $mapel["tanggal"],
                 "jam_mulai" => $mapel["jam_mulai"],
                 "jam_selesai" => $mapel["jam_selesai"],
-                "kode_guru" => $mapel["users_kode_guru"]
+                "kode_guru" => $mapel["kode_guru"],
             ]);
         }
         echo json_encode($data);
-    } else if($_GET['type'] == "single") {
-        $id = mysqli_escape_string($db, $_GET['id']);
-        $query = mysqli_query($db, "SELECT * FROM mata_pelajaran WHERE id = $id");
-    
-        while ($mapel = mysqli_fetch_array($query)) {
-            echo json_encode([
-                "id" => $mapel["id"],
-                "nama_mapel" => $mapel["nama_mapel"],
-                "kode_mapel" => $mapel["kode_mapel"],
-                "jam_mulai" => $mapel["jam_mulai"],
-                "jam_selesai" => $mapel["jam_selesai"],
-                "kode_guru" => $mapel["users_kode_guru"]
-            ]);
-            echo "\n";
-            break;
-        }
-    }
+    } 
 
 }else{
     die("Method not allowed");
